d25cea0392150809d8d733a722d3cd661afc5938
Owner <minemidnightmc@gmail.com>
2019-04-13 00:02:32 -0400

    Bypass message queue if opcode is identify

--- src/gateway/Shard.js
+++ src/gateway/Shard.js
@@ -55,7 +55,7 @@ class Shard extends EventEmitter {
 
 	async send(data) {
 		console.log("sending", data);
-		if(this.status !== "ready") this.messageQueue.push(data);
+		if(this.status !== "ready" && data.op !== constants.OPCODES.IDENTIFY) this.messageQueue.push(data);
 		else this.ws.send(this.compressionHandler.compress(data));
 	}
 
@@ -276,17 +276,18 @@ class Shard extends EventEmitter {
 			op: constants.OPCODES.IDENTIFY,
 			d: {
 				token: this.token,
+				large_threshold: 50,
 				properties: {
 					$os: process.platform,
-					$browser: "Oxyl",
-					$device: "Oxyl"
+					$browser: "oxyl-sharder",
+					$device: "oxyl-sharder"
 				},
 				shard: [this.id, this.shardCount],
 				presence: {
 					since: null,
 					game: {
 						name: "o!help",
-						type: 0
+						type: 2
 					},
 					status: "online",
 					afk: false
